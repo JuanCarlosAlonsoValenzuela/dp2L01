6a57d834a75635129881af4e0aafb0c65aed133f
Roberto Hermoso <rhermoso98@gmail.com>
2019-02-22 13:58:31 +0100

    Messages Fix

--- src/main/java/repositories/BoxRepository.java
+++ src/main/java/repositories/BoxRepository.java
@@ -15,16 +15,16 @@ import domain.Message;
 public interface BoxRepository extends JpaRepository<Box, Integer> {
 
 	//
-	@Query("select b from Actor a inner join a.boxes b where b.name = 'Received messages' and a = ?1")
+	@Query("select b from Actor a inner join a.boxes b where b.name = 'INBOX' and a = ?1")
 	public Box getRecievedBoxByActor(Actor actor);
 
-	@Query("select b from Actor a inner join a.boxes b where b.name = 'Spam' and a = ?1")
+	@Query("select b from Actor a inner join a.boxes b where b.name = 'SPAMBOX' and a = ?1")
 	public Box getSpamBoxByActor(Actor actor);
 
-	@Query("select b from Actor a inner join a.boxes b where b.name = 'Trash' and a = ?1")
+	@Query("select b from Actor a inner join a.boxes b where b.name = 'TRASHBOX' and a = ?1")
 	public Box getTrashBoxByActor(Actor actor);
 
-	@Query("select b from Actor a inner join a.boxes b where b.name = 'Notification' and a = ?1")
+	@Query("select b from Actor a inner join a.boxes b where b.name = 'NOTIFICATIONBOX' and a = ?1")
 	public Box getNotificationBoxByActor(Actor actor);
 
 	@Query("select b from Actor a inner join a.boxes b where b.fatherBox =?1")
@@ -33,7 +33,7 @@ public interface BoxRepository extends JpaRepository<Box, Integer> {
 	@Query("select b from Box b inner join b.messages f where f LIKE ?1")
 	public List<Box> getCurrentBoxByMessage(Message m);
 
-	@Query("select b from Actor a inner join a.boxes b where b.name = 'Sent messages' and a = ?1")
+	@Query("select b from Actor a inner join a.boxes b where b.name = 'OUTBOX' and a = ?1")
 	public Box getSentBoxByActor(Actor actor);
 
 }
